1a4c9355da155be848e2dd791f09b746b3418878
Jordan Scales <scalesjordan@gmail.com>
2014-02-09 14:49:44 -0500

    generate a random hash if one does not exist

--- js/init.js
+++ js/init.js
@@ -1,3 +1,22 @@
 var FIREBASE_ROOT = 'https://trackd-fb.firebaseio.com/boards/';
-var BOARD_ID = document.location.hash.slice(1) || 'noid';
+var HASH_LENGTH = 6;
+
+if (document.location.hash.length < HASH_LENGTH) {
+  document.location.hash = randomHash();
+}
+
+var BOARD_ID = document.location.hash.slice(1);
 var boardRef = new Firebase(FIREBASE_ROOT + BOARD_ID);
+
+function randomHash(length) {
+  length = length || HASH_LENGTH;
+
+  var chars = '0123456789abcdef';
+  var i, build = '';
+
+  for (i = 0; i < length; i++) {
+    build += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return build;
+}
